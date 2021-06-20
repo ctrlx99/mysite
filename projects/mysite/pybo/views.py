ef88188f8c3d9e1c5e850711864afd741d4156b5
LeeSinWoo <ctrlx99@naver.com>
2021-06-20 13:21:02 +0900

    html파일 변경 추가

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 
 def index(request):
@@ -34,8 +34,18 @@ def answer_create(request, question_id):
     pybo 답변 등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone()
+            answer.question = question
+            answer.sava()
+            return redirect('pybo:detail' question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 
 def question_create(request):
@@ -55,3 +65,4 @@ def question_create(request):
     return render(request, 'pybo/question_form.html', context)
 
 
+class
