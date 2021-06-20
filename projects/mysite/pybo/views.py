c7f0174c34f46e269f4179824d890d8ffb49aaa2
LeeSinWoo <ctrlx99@naver.com>
2021-06-20 20:46:23 +0900

    141page 실습

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 from django.http import HttpResponse
@@ -13,8 +13,18 @@ def index(request):
     """
     pybo 목록 출력
     """
+
+    # 입력인자
+    page = request.GET.get('page', '1')  # 페이지
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+
+    # 페이징 처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여 주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
     return render(request, 'pybo/question_list.html', context)
 
