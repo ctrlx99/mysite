ce95582201b4ce59a39d3e052cc4f6047e9f5b68
LeeSinWoo <ctrlx99@naver.com>
2021-10-04 18:48:07 +0900

    216page 실습

--- projects/mysite/pybo/views/base_views.py
+++ projects/mysite/pybo/views/base_views.py
@@ -29,7 +29,8 @@ def detail(request, question_id):
     """
     pybo 내용 출력
     """
-    #question = Question.objects.get(id=question_id)
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
