a711536b0804594d9c9f0298a12d229d09a2002f
LeeSinWoo <ctrlx99@naver.com>
2021-10-04 20:30:24 +0900

    239page 실습

--- projects/mysite/pybo/views/base_views.py
+++ projects/mysite/pybo/views/base_views.py
@@ -1,8 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-
+from django.db.models import Q
 from ..models import Question
-
 
 
 def index(request):
@@ -12,15 +11,23 @@ def index(request):
 
     # 입력인자
     page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')       # 검색어
 
     # 조회
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |                  # 제목 검색
+            Q(content__icontains=kw) |                  # 내용 검색
+            Q(author__username__icontains=kw) |        # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)   # 답변 글쓴이 검색
+        ).distinct()
 
     # 페이징 처리
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여 주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}  # page와 kw가 추가됨
     # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
     return render(request, 'pybo/question_list.html', context)
 
